ae1d4a27cb19a2c93b9edee65edb41871ad0b28f
SaldanhaVitor <vitorgtfo@gmail.com>
2022-09-03 23:02:15 -0300

    chore: add mongodb jest in memory

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: [ '<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**', '!**/test/**' ],  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**', '!**/test/**'], coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
-  }
+  },
+  preset: "@shelf/jest-mongodb"
 }
